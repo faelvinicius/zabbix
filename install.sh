b0b0979382de59f94607c679e9c153dcd804db34
faelvinicius <49661419+faelvinicius@users.noreply.github.com>
2019-04-16 13:30:27 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ if ! mkdir -p /zbx-server/mysql/data
 then
     echo "Não foi possível criar o diretorio."
     exit 1
+fi    
 
 echo "Digite o IP do para prosseguir com a instalacao do MYSQL..."
 read ip
@@ -38,19 +39,21 @@ if ! docker container run -d -p 3306:3306 -v /zbx-server/mysql/data:/var/lib/mys
 then
     echo "Não foi instalar o ZABBIX."
     exit 1
+fi
 
 echo "Instalando Zabbix Server..."
 if ! docker container run -d -p 10051:10051 -e MYSQL_ROOT_PASSWORD=zabbix -e DB_SERVER_HOST=$ip -e DB_SERVER_PORT=3306 -e MYSQL_USER=zabbix -e MYSQL_PASSWORD=zabbix -e MYSQL_DATABASE=zabbix --name zbx-server zabbix/zabbix-server-mysql:ubuntu-4.0-latest
 then
     echo "Não foi possível instalar o zabbix."
     exit 1	
-	
+fi
+
 echo "Instalando zabbix web..."
 if ! docker container run -d -p 80:80 -e DB_SERVER_HOST=$ip -e DB_SERVER_PORT=3306 -e MYSQL_USER=zabbix -e MYSQL_PASSWORD=zabbix -e MYSQL_DATABASE=zabbix -e ZBX_SERVER_HOST=$ip -e PHP_TZ=America/Sao_Paulo --name zbx-web zabbix/zabbix-web-apache-mysql:ubuntu-4.0-latest
 then
     echo "Não foi possível instalar o zabbix web."
     exit 1
-
+fi
 	
 echo "Instalação finalizada com sucesso"
 echo "Basta acessar pelo browser o seguinte endereço $ip "
