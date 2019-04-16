91530c14413981fb727b28f47907829388621b0b
faelvinicius <49661419+faelvinicius@users.noreply.github.com>
2019-04-16 15:04:30 -0300

    Update zabbix_correios.sh

--- zabbix_correios.sh
+++ zabbix_correios.sh
@@ -61,7 +61,7 @@ then
 fi
 
 echo "Instalacao API Correios"
-if ! docker run --rm -d -p 8080:8080 feliperoberto/non-official-correios-api
+if ! docker run --rm -d -p 80:8080 feliperoberto/non-official-correios-api
 then
  	   echo "Nao foi possivel instalar a API correios."
        exit 1
