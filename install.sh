4830a00de164bfcdd15f52d76dfe555156af1528
faelvinicius <49661419+faelvinicius@users.noreply.github.com>
2019-04-16 17:22:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Autor: Rafael Vinicius
+
+################################### ATUALIZANDO O SO ####################################
 echo "Iniciando a instalacao do Zabbix..."
 echo "Atualizando repositórios..."
 if ! apt-get update
@@ -9,6 +12,8 @@ then
 fi
 echo "Atualização feita com sucesso"
 
+
+#################################### ATUALIZANDO CURL #####################################
 echo "Instalando o curl..."
 if ! apt-get install curl -y
 then
@@ -17,12 +22,16 @@ then
 fi
 echo "Instalação do curl feita com sucesso"
 
+##################################### INSTALANDO DOCKER ####################################
+
 echo "Instalando o Docker..."
 if ! curl -fsSL https://get.docker.com | bash
 then
     echo "Não foi possível instalar o docker."
     exit 1
 fi
+
+##################################### CRIANDO DIRETORIO PARA O MYSQL ########################
 
 echo "Criando diretorio para instalacao do MYSQL..."
 if ! mkdir -p /zbx-server/mysql/data
@@ -31,7 +40,9 @@ then
     exit 1
 fi    
 	echo "Diretorio criado com sucesso!"
-	
+
+##################################### BAIXANDO CONTAINER MYSQL ###############################
+
 echo "Digite o IP do HOST para prosseguir com a instalacao do MYSQL..."
 read ip
 if ! docker container run -d -p 3306:3306 -v /zbx-server/mysql/data:/var/lib/mysql -e MYSQL_HOST=$ip -e MYSQL_USER=zabbix -e MYSQL_ROOT_PASSWORD=zabbix -e MYSQL_DATABASE=zabbix -e MYSQL_PASSWORD=zabbix --name mysql-zbx mysql:5.7
@@ -41,8 +52,8 @@ then
 fi
 	echo "MYSQL instalado com sucesso!"
 
-echo " Digite o IP do HOST para prosseguir a instalacao do Zabbix Server..."
-read ip
+##################################### BAIXANDO CONTAINER ZABBIX SERVER #########################
+
 if ! docker container run -d -p 10051:10051 -e MYSQL_ROOT_PASSWORD=zabbix -e DB_SERVER_HOST=$ip -e DB_SERVER_PORT=3306 -e MYSQL_USER=zabbix -e MYSQL_PASSWORD=zabbix -e MYSQL_DATABASE=zabbix --name zbx-server zabbix/zabbix-server-mysql:ubuntu-4.0-latest
 then
 	echo "Nao foi possivel instalar Zabbix Server."
@@ -50,8 +61,8 @@ then
 fi
 	echo "Zabbix server instalado com sucesso!"
 
-echo "Digite o IP do HOST para prosseguir a instalacao do zabbix web..."
-read ip
+###################################### BAIXANDO CONTAINER ZABBIX WEB ###########################
+
 if ! docker container run -d -p 80:80 -e DB_SERVER_HOST=$ip -e DB_SERVER_PORT=3306 -e MYSQL_USER=zabbix -e MYSQL_PASSWORD=zabbix -e MYSQL_DATABASE=zabbix -e ZBX_SERVER_HOST=$ip -e PHP_TZ=America/Sao_Paulo --name zbx-web zabbix/zabbix-web-apache-mysql:ubuntu-4.0-latest
  then
  	echo "Nao foi possivel instalar zabbix web."
