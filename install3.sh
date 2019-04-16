0673bc6ff65176a72803c6a94a760943a1492d93
faelvinicius <49661419+faelvinicius@users.noreply.github.com>
2019-04-16 14:15:08 -0300

    Update and rename install2.sh to install3.sh

--- install3.sh
+++ install3.sh
@@ -36,24 +36,18 @@ fi
 echo "Digite o IP do para prosseguir com a instalacao do MYSQL..."
 read ip
 if ! docker container run -d -p 3306:3306 -v /zbx-server/mysql/data:/var/lib/mysql -e MYSQL_HOST=$ip -e MYSQL_USER=zabbix -e MYSQL_ROOT_PASSWORD=zabbix -e MYSQL_DATABASE=zabbix -e MYSQL_PASSWORD=zabbix --name mysql-zbx mysql:5.7
-then
-    echo "Não foi instalar o ZABBIX."
     exit 1
 fi
 
 echo " Digite o IP do HOST para prosseguir a instalacao do Zabbix Server..."
 read ip
 if ! docker container run -d -p 10051:10051 -e MYSQL_ROOT_PASSWORD=zabbix -e DB_SERVER_HOST=$ip -e DB_SERVER_PORT=3306 -e MYSQL_USER=zabbix -e MYSQL_PASSWORD=zabbix -e MYSQL_DATABASE=zabbix --name zbx-server zabbix/zabbix-server-mysql:ubuntu-4.0-latest
-then
-    echo "Não foi possível instalar o zabbix."
     exit 1	
 fi
 
 echo "Digite o IP do HOST para prosseguir a instalacao do zabbix web..."
 read ip
 if ! docker container run -d -p 80:80 -e DB_SERVER_HOST=$ip -e DB_SERVER_PORT=3306 -e MYSQL_USER=zabbix -e MYSQL_PASSWORD=zabbix -e MYSQL_DATABASE=zabbix -e ZBX_SERVER_HOST=$ip -e PHP_TZ=America/Sao_Paulo --name zbx-web zabbix/zabbix-web-apache-mysql:ubuntu-4.0-latest
-then
-    echo "Não foi possível instalar o zabbix web."
     exit 1
 fi
 	
